99dc17f910eef15e8ebefe94b8f0dd0d529ff3eb
Marcus Chong You Long <mugurox@gmail.com>
2017-04-27 14:40:59 +0800

    Marcus

--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,18 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
+    ArrayAdapter ma;
+    ArrayList<Module> module;
 
 
     @Override
@@ -17,14 +22,20 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        lv = (ListView) this.findViewById(R.id.lvModules);
+        lv = (ListView) this.findViewById(R.id.lvModule);
         tvYear = (TextView) findViewById(R.id.tvYear);
 
         Intent i = getIntent();
         String year = i.getStringExtra("year");
         tvYear.setText(year);
 
+        module = new ArrayList<>();
+        module.add(new Module("C208", true));
+        module.add(new Module("C200", false));
+        module.add(new Module("C346", true));
 
+        ma = new ModuleAdapter(this, R.layout.row, module);
+        lv.setAdapter(ma);
 
     }
 
