c90eb29262915445c5840de4b418da5bc79ae2ca
15017274 <15017274@myrp.edu.sg>
2017-04-27 14:35:03 +0800

    JJ

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,5 +1,6 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,7 +14,6 @@ public class FirstActivity extends AppCompatActivity {
 
     ListView lv;
     ArrayList<String> al;
-    ArrayList<Module> module;
     ArrayAdapter<String> aa;
 
     @Override
@@ -24,25 +24,29 @@ public class FirstActivity extends AppCompatActivity {
 
         lv = (ListView) this.findViewById(R.id.lvModule);
         al = new ArrayList<String>();
+        lv.setAdapter(aa);
         al.add("Year 1");
         al.add("Year 2");
         al.add("Year 3");
+        aa.notifyDataSetChanged();
 
-        module = new ArrayList<Module>();
-        module.add(new Module("C208", true));
-        module.add(new Module("C200", false));
-        module.add(new Module("C346", true));
-
-
-        aa = new ModuleAdapter(this, R.layout.row, module);
-        lv.setAdapter(aa);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(FirstActivity.this,
+                        SecondActivity.class);
+                // Pass the String array holding the name & age to new activity
+                i.putExtra("year", al.get(position));
 
+                // Start the new activity
+                startActivity(i);
             }
         });
     }
 }
+
+
+
+
